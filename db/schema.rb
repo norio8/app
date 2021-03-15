e1aa4105b31f32b41b0d1a47f45a99aabd188e9e
norio8 <70767059+norio8@users.noreply.github.com>
2021-03-15 12:47:09 +0900

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_024810) do
+ActiveRecord::Schema.define(version: 2021_03_15_034401) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "capital"
   end
 
 end
