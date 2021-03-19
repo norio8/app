bd3ac314c30f89409acd78faee9c40c85c553c31
norio8 <70767059+norio8@users.noreply.github.com>
2021-03-19 16:35:30 +0900

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name , length: { maximum: 30 } , presence: true
+  validates :age , length: { maximum: 2 } , presence: true
+  validates :capital ,length: { maximum: 30} 
 end
